7b9f0fa53491897b3869b26e0fd8864f64e746cb
Tim Kist <timkist@imagitech.co.uk>
2016-08-05 10:54:48 +0100

    Allow touch and scale gestures at same time

--- app/src/main/java/com/israelferrer/smokeandmirrors/GooglePhotosActivity.java
+++ app/src/main/java/com/israelferrer/smokeandmirrors/GooglePhotosActivity.java
@@ -5,7 +5,7 @@ import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.util.Log;
+import android.view.GestureDetector;
 import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.ScaleGestureDetector;
@@ -128,15 +128,29 @@ public class GooglePhotosActivity extends AppCompatActivity {
 //                        }
                     }
                 });
+
+        final GestureDetector tapGestureDetector = new GestureDetector(this,
+                new GestureDetector.SimpleOnGestureListener() {
+                    @Override
+                    public boolean onSingleTapUp(MotionEvent e) {
+                        transition();
+                        return true;
+                    }
+                });
         smallRecyclerView.addOnItemTouchListener(new RecyclerView.OnItemTouchListener() {
             @Override
             public boolean onInterceptTouchEvent(RecyclerView rv, MotionEvent e) {
-                return true;
+                scaleGestureDetector.onTouchEvent(e);
+                return tapGestureDetector.onTouchEvent(e) || scaleGestureDetector.isInProgress();
             }
 
             @Override
             public void onTouchEvent(RecyclerView rv, MotionEvent e) {
+                if (scaleGestureDetector.isInProgress()) {
                 scaleGestureDetector.onTouchEvent(e);
+                } else {
+                    tapGestureDetector.onTouchEvent(e);
+                }
             }
 
             @Override
